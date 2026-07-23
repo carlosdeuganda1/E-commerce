b76032f4aa913fb08494d37375bb64d3ee977330
Carlos De Uganda <carlosdeuganda@gmail.com>
2026-07-23 04:18:24 +0300

    Add Login, Register, and Forgot Password routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,26 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import HomePage from './pages/HomePage';
 import ProductListing from './pages/ProductListing';
-import ProductDetails from './pages/ProductDetails';  // NEW
-import CartPage from './pages/CartPage';              // NEW
+import ProductDetails from './pages/ProductDetails';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
 
 function App() {
   return (
     <Layout>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductListing />} />
-        <Route path="/product/:id" element={<ProductDetails />} />  {/* NEW */}
-        <Route path="/cart" element={<CartPage />} />              {/* NEW */}
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<CartPage />} />
+        
+        {/* Auth Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       </Routes>
     </Layout>
   );
