7d03c5cd8b6580b18d4b197381a3a5c35cba0a93
Carlos De Uganda <carlosdeuganda@gmail.com>
2026-07-23 04:07:23 +0300

    Add ProductDetails and CartPage routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import HomePage from './pages/HomePage';
 import ProductListing from './pages/ProductListing';
+import ProductDetails from './pages/ProductDetails';  // NEW
+import CartPage from './pages/CartPage';              // NEW
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductListing />} />
+        <Route path="/product/:id" element={<ProductDetails />} />  {/* NEW */}
+        <Route path="/cart" element={<CartPage />} />              {/* NEW */}
       </Routes>
     </Layout>
   );
